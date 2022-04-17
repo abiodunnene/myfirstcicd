a550571d06933cead6a4362756a70822f5c24402
abiodunnene <102881019+abiodunnene@users.noreply.github.com>
2022-04-16 22:47:15 -0500

    firstproject

---
+++ mycicd/src/main/java/com/itexpect/marketing/mycicd/Employee.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itexpect.marketing.mycicd;
+
+/**
+ *
+ * @author 18477
+ */
+public class Employee {
+
+    private int id;
+    private String first;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", first=" + first + '}';
+
+    }
+
+    public Employee(int id, String first) {
+        this.id = id;
+        this.first = first;
+    }
+
+}
---
+++ mycicd/src/test/java/test/com/EmployeeTest.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package test.com;
+
+import com.itexpect.marketing.mycicd.Employee;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author 18477
+ */
+public class EmployeeTest {
+    
+    Employee emp;
+    
+    
+    public EmployeeTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void testFirst() {
+    
+        emp= new Employee(1, "Tom");
+        assertEquals(1, emp.getId());
+        assertEquals("Tom", emp.getFirst());
+        
+    
+    }
+}
